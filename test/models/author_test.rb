0de36584eb16604cb1b7ae4f8865dbfd38defe6a
Maria <mliberov@gmail.com>
2015-02-18 22:19:51 -0500

    Make fixtures test pass

--- test/models/author_test.rb
+++ test/models/author_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class AuthorTest < ActiveSupport::TestCase
+  setup do
+  end
+
   test "the truth" do
     assert true
+    refute false
+  end
+
+  test "emails are unique" do
+    bad_chris = Author.new(name: "Not Chris", email: "chris@awesome.com")
+    refute bad_chris.save
   end
 end

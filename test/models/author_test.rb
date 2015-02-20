7946a58749e019e725f276d92a651b239dc0e4a3
Maria <mliberov@gmail.com>
2015-02-19 22:08:41 -0500

    Add code

--- test/models/author_test.rb
+++ test/models/author_test.rb
@@ -9,6 +9,14 @@ class AuthorTest < ActiveSupport::TestCase
     refute false
   end
 
+  test "there are authors" do
+    assert Author.count > 0
+  end
+
+  test "chris is chris" do
+    assert_equal "Chris", authors(:chris).name
+  end
+
   test "emails are unique" do
     bad_chris = Author.new(name: "Not Chris", email: "chris@awesome.com")
     refute bad_chris.save
